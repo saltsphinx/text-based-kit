980dc2f850227d1c38cd6aa461f5fb7bcfcd4111
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-05-26 23:14:56 -0500

    Add imports and exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const elementSpacing = require('./elementSpacing');
+const logInterface = require('./logInterface');
+const config = require('./config');
+
+module.exports = {
+  logInterface,
+  config,
+};
