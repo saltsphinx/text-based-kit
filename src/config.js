b41c33ac209a074f3efb7373001f5e72323f89dd
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-05-26 23:14:00 -0500

    Add config file

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  msgElement: 'p',
+  background: '#000',
+  foreground: '#222',
+  textColor: '#FFFFFF99',
+  textBackground: 'transparent',
+  fontSize: '0.95rem',
+  fontWeight: '400',
+  lineBreakHeight: '0.5rem',
+  lineBreakCount: 1,
+};
